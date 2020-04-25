5810b0101400034aa362870277f87d5fc10a8d07
nikolamiljkovic91 <nikolamiljkovic91@gmail.com>
2020-04-25 17:32:26 +0200

    added post routes

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -6,7 +6,7 @@ const User = require('../../models/User');
 const Post = require('../../models/Post');
 
 //@route    POST api/posts
-//@desc     Create a post
+//@desc     Create post
 //@access   Private
 
 router.post(
@@ -119,7 +119,7 @@ router.delete('/:id', auth, async (req, res) => {
 });
 
 //@route    PUT api/posts/like/:id
-//@desc     Like a post
+//@desc     Like post
 //@access   Private
 
 router.put('/like/:id', auth, async (req, res) => {
@@ -151,7 +151,7 @@ router.put('/like/:id', auth, async (req, res) => {
 });
 
 //@route    PUT api/posts/dislike/:id
-//@desc     Dislike a post
+//@desc     Dislike post
 //@access   Private
 
 router.put('/dislike/:id', auth, async (req, res) => {
@@ -176,6 +176,164 @@ router.put('/dislike/:id', auth, async (req, res) => {
     await post.save();
 
     res.json(post.dislikes);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server Error');
+  }
+});
+
+//@route    PUT api/posts/comment/:id
+//@desc     Create comment
+//@access   Private
+
+router.put(
+  '/comment/:post_id',
+  auth,
+  [check('text', 'Text is required').not().isEmpty()],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    try {
+      const user = await User.findById(req.user.id);
+      const post = await Post.findById(req.params.post_id);
+
+      const newComment = {
+        user: req.user.id,
+        text: req.body.text,
+        username: user.username,
+      };
+
+      post.comments.unshift(newComment);
+
+      await post.save();
+
+      res.json(post.comments);
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send('Server Error');
+    }
+  }
+);
+
+//@route    DELETE api/posts/comment/:id
+//@desc     Delete comment
+//@access   Private
+
+router.delete('/comment/:post_id/:comment_id', auth, async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.post_id);
+
+    // Pull the comment from the post
+    const comment = post.comments.find(
+      (comment) => comment.id === req.params.comment_id
+    );
+
+    // Make sure comment exists
+    if (!comment) {
+      return res.status(404).json({ msg: 'Comment not found' });
+    }
+
+    // Make sure user is authorized
+    if (comment.user.toString() !== req.user.id) {
+      return res.status(401).json({ msg: 'User not authorized' });
+    }
+
+    const removeIndex = post.comments
+      .map((comment) => comment.id)
+      .indexOf(req.params.comment_id);
+
+    post.comments.splice(removeIndex, 1);
+
+    await post.save();
+
+    res.json(post.comments);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server Error');
+  }
+});
+
+//@route    PUT api/posts/like/comment/:comment_id
+//@desc     Like comment
+//@access   Private
+
+router.put('/like/:post_id/:comment_id', auth, async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.post_id);
+
+    // Pull the comment from the post
+    const comment = post.comments.find(
+      (comment) => comment.id === req.params.comment_id
+    );
+
+    // Make sure comment exists
+    if (!comment) {
+      return res.status(404).json({ msg: 'Comment not found' });
+    }
+
+    // Check if the comment has already been liked
+    if (
+      comment.likes.filter((like) => like.user.toString() === req.user.id)
+        .length > 0
+    ) {
+      //   return res.status(400).json({ msg: 'Post already liked' });
+      const removeIndex = comment.likes
+        .map((like) => like.user.toString())
+        .indexOf(req.user.id);
+
+      comment.likes.splice(removeIndex, 1);
+    } else {
+      comment.likes.unshift({ user: req.user.id });
+    }
+
+    await post.save();
+
+    res.json(post);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server Error');
+  }
+});
+
+//@route    PUT api/posts/dislike/comment/:comment_id
+//@desc     Dislike comment
+//@access   Private
+
+router.put('/dislike/:post_id/:comment_id', auth, async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.post_id);
+
+    // Pull the comment from the post
+    const comment = post.comments.find(
+      (comment) => comment.id === req.params.comment_id
+    );
+
+    // Make sure comment exists
+    if (!comment) {
+      return res.status(404).json({ msg: 'Comment not found' });
+    }
+
+    // Check if the comment has already been liked
+    if (
+      comment.dislikes.filter(
+        (dislike) => dislike.user.toString() === req.user.id
+      ).length > 0
+    ) {
+      //   return res.status(400).json({ msg: 'Post already liked' });
+      const removeIndex = comment.dislikes
+        .map((like) => like.user.toString())
+        .indexOf(req.user.id);
+
+      comment.dislikes.splice(removeIndex, 1);
+    } else {
+      comment.dislikes.unshift({ user: req.user.id });
+    }
+
+    await post.save();
+
+    res.json(post);
   } catch (err) {
     console.error(err.message);
     res.status(500).send('Server Error');
