62d24e28614f2d2309a76e64ce8004f0a119cc28
nikolamiljkovic91 <nikolamiljkovic91@gmail.com>
2020-04-30 20:02:13 +0200

    profile update fixed

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -14,7 +14,6 @@ router.post(
   auth,
   [check('text', 'Text is required').not().isEmpty()],
   async (req, res) => {
-    console.log(req);
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
       return res.status(400).json({ errors: errors.array() });
